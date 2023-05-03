972fb26b97b97e46bc7013383e10b03a1a0f0738
zynkd <coding707@mailbox.org>
2023-05-03 10:18:42 +0200

    Added color selector for the counter

--- src/App.js
+++ src/App.js
@@ -2,14 +2,23 @@ import React, { useState } from 'react';
 
 function App() {
   const [count, setCount] = useState(0);
+  const [color, setColor] = useState('');
 
   const handleIncreaseByOne = () => {
     setCount((prev) => prev + 1);
   };
 
+  const handleIncreaseBy100 = () => {
+    setCount((prev) => prev + 100);
+  };
+
+  const handleColorSelect = (event) => {
+    setColor(event.target.value);
+  };
+
   const handleFormSubmit = (event) => {
     event.preventDefault();
-  }
+  };
 
   return (
     <div className='h-screen flex items-center justify-center'>
@@ -18,20 +27,54 @@ function App() {
           Simple counter form
         </h1>
         <h2 className='form-counter mb-2 text-lg font-bold text-center'>
-          {`Current counter ${count}`}
+          Current counter:{' '}
+          <span
+            style={{
+              color: color,
+            }}
+          >
+            {count}
+          </span>
         </h2>
-        <input
-          type='button'
-          value='Counter++'
-          className='bg-blue-500 text-white px-4 py-2 font-bold rounded-md m-2'
-          onClick={handleIncreaseByOne}
-        />
-        <button
-          className='bg-blue-500 text-white px-4 py-2 font-bold rounded-md m-2'
-          onClick={handleIncreaseByOne}
-        >
-          Counter [+1]
-        </button>
+        <div className='flex justify-around'>
+          <input
+            type='button'
+            value='Counter+1'
+            className='bg-blue-500 text-white px-4 py-2 font-bold rounded-md m-2'
+            onClick={handleIncreaseByOne}
+          />
+          <button
+            className='bg-blue-500 text-white px-4 py-2 font-bold rounded-md m-2'
+            onClick={handleIncreaseByOne}
+          >
+            Counter [+1]
+          </button>
+        </div>
+        <div className='flex justify-around mb-3'>
+          <input
+            type='button'
+            value='Counter+100'
+            className='bg-blue-500 text-white px-4 py-2 font-bold rounded-md m-2'
+            onClick={handleIncreaseBy100}
+          />
+          <button
+            className='bg-blue-500 text-white px-4 py-2 font-bold rounded-md m-2'
+            onClick={handleIncreaseBy100}
+          >
+            Counter [+100]
+          </button>
+        </div>
+
+        <div className='flex justify-center gap-4'>
+          <p className='mb-1 text-center'>Counter color:</p>
+          <select value={color} onChange={handleColorSelect}>
+            <option value=''>Choose color</option>
+            <option value='red'>Red</option>
+            <option value='green'>Green</option>
+            <option value='blue'>Blue</option>
+            <option value='purple'>Purple</option>
+          </select>
+        </div>
       </form>
     </div>
   );
