445394129c4035e4b63968ac40008afc76c90267
zynkd <coding707@mailbox.org>
2023-06-02 18:03:16 +0200

    Simlified code to use `Alert` component, removed unnecessary state

--- src/App.js
+++ src/App.js
@@ -9,21 +9,20 @@ const API_URL = 'https://api.sampleapis.com/csscolornames/colors';
 const DEFAULT_BACKGROUND_COLOR = '#3B82F6';
 
 function App() {
+  const [colorListFromServer, setColorListFromServer] = useState(null);
+
   const [count, setCount] = useState(0);
   const [increment, setIncrement] = useState(1);
-
   const [customIncrement, setCustomIncrement] = useState('');
 
   const [color, setColor] = useState(DEFAULT_BACKGROUND_COLOR);
-
   const [textColor, setTextColor] = useState('');
-  const [colorListFromServer, setColorListFromServer] = useState(null);
 
-  // Errors when choosing custom counter increment
-  const [hasErrorsCustomIncrement, setHasErrorsCustomIncrement] =
-    useState(false);
   const [errorMessageCustomIncrement, setErrorMessageCustomIncrement] =
-    useState('');
+  useState('');
+  // const hasErrorsCustomIncrement =
+  //   errorMessageCustomIncrement === '' ? false : true;
+  const hasErrorsCustomIncrement = Boolean(errorMessageCustomIncrement);
 
   // Errors when trying type or submit a new color
   const [hasErrorTextColor, setHasErrorTextColor] = useState(false);
@@ -41,7 +40,55 @@ function App() {
     setTimeout(() => errorSetter(false), timer);
   };
 
-  const handleTextColor = (e) => {
+  // Handlers for Counter
+  const handleCounterIncreaseButton = () => {
+    setCount((prev) => prev + increment);
+  };
+
+  const handleCounterResetButton = () => {
+    setCount(0);
+    setIncrement(1);
+    setCustomIncrement('');
+    setColor(DEFAULT_BACKGROUND_COLOR);
+  };
+
+  // Handlers for Increments
+  const handleIncrementTextInput = (e) => {
+    const typedInput = e.target.value;
+    setCustomIncrement(typedInput);
+
+    if (typedInput === '') {
+      setErrorMessageCustomIncrement('');
+    } else if (isNaN(typedInput)) {
+      setErrorMessageCustomIncrement('This is not a number');
+    } else if (typedInput % 1 !== 0) {
+      setErrorMessageCustomIncrement('Decimal places are not allowed');
+    } else if (typedInput < 1 || typedInput > 100) {
+      setErrorMessageCustomIncrement('Numbers must be between 1 and 100');
+    } else {
+      setErrorMessageCustomIncrement('');
+    }
+  };
+
+  const handleIncrementSubmit = () => {
+    if (!hasErrorsCustomIncrement && customIncrement !== '') {
+      setIncrement(+customIncrement);
+    }
+
+    setCustomIncrement('');
+  };
+
+  const handleIncrementSelect = (e) => {
+    setIncrement(+e.target.value);
+  };
+
+  // Handlers for Colors
+
+  const handleColorSelect = (e) => {
+    setColor(e.target.value);
+  };
+
+  const handleColorTextInput = (e) => {
     const typedInput = e.target.value;
     setTextColor(typedInput);
 
@@ -52,7 +99,7 @@ function App() {
     }
   };
 
-  const handleSubmitColor = () => {
+  const handleColorSubmit = () => {
     const foundColor = colorListFromServer.find(
       (item) => item.name === textColor,
     );
@@ -64,56 +111,10 @@ function App() {
       : displayTempErrorMessage(setHasErrorSubmitColor, 3500);
   };
 
-  const handleSelectIncrement = (e) => {
-    setIncrement(+e.target.value);
-  };
+  // Handler for Form
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
-  };
-
-  const handleClickIncreaseByIncrement = () => {
-    setCount((prev) => prev + increment);
-  };
-
-  const handleResetCounter = () => {
-    setCount(0);
-    setIncrement(1);
-    setCustomIncrement('');
-    setColor(DEFAULT_BACKGROUND_COLOR);
-    setHasErrorsCustomIncrement(false);
-  };
-
-  const handleSelectColor = (e) => {
-    setColor(e.target.value);
-  };
-
-  const handleTextCustomIncrement = (e) => {
-    const typedInput = e.target.value;
-    setCustomIncrement(typedInput);
-
-    if (typedInput === '') {
-      setHasErrorsCustomIncrement(false);
-    } else if (isNaN(typedInput)) {
-      setErrorMessageCustomIncrement('This is not a number');
-      setHasErrorsCustomIncrement(true);
-    } else if (typedInput % 1 !== 0) {
-      setErrorMessageCustomIncrement('Decimal places are not allowed');
-      setHasErrorsCustomIncrement(true);
-    } else if (typedInput < 1 || typedInput > 100) {
-      setErrorMessageCustomIncrement('Numbers must be between 1 and 100');
-      setHasErrorsCustomIncrement(true);
-    } else {
-      setHasErrorsCustomIncrement(false);
-    }
-  };
-
-  const handleSubmitCustomIncrement = () => {
-    if (hasErrorsCustomIncrement === false && customIncrement !== '') {
-      setIncrement(+customIncrement);
-    }
-
-    setCustomIncrement('');
   };
 
   function handleOnMouseEnter(event) {
@@ -126,186 +127,184 @@ function App() {
   }
 
   return (
-    <div className='h-screen flex flex-col items-center justify-center'>
-      <form className='border-8 px-20 py-12' onSubmit={handleFormSubmit}>
-        <h1 className='form-title mb-4 text-3xl font-bold'>
-          Zynk's Simple Counter
-        </h1>
+    <div className='max-w-[540px] mx-auto px-2'>
+      <main className='h-screen flex flex-col items-center justify-center text-center'>
+        <form
+          className='px-4 py-8 pb-2 sm:px-16 lg:px-24 lg:py-10 lg:pb-6 border-0 sm:border-8 md:border-8'
+          onSubmit={handleFormSubmit}
+        >
+          <section>
+            <h1 className='form-title mb-4 text-3xl font-bold'>
+              Zbynek's Counter
+            </h1>
+          </section>
 
-        <div className='text-center text-md mb-6'>
-          <h2 className='mb-1 text-lg font-bold'>{`Counter: ${count}`}</h2>
-          <div className='flex justify-center mb-4'>
-            <button
-              type='button'
-              className='text-sm text-white px-4 py-2 rounded-md m-2'
-              style={{
-                backgroundColor: color ? color : DEFAULT_BACKGROUND_COLOR,
-              }}
-              onMouseEnter={handleOnMouseEnter}
-              onMouseLeave={handleOnMouseLeave}
-              onClick={handleClickIncreaseByIncrement}
-            >
-              Increase +{increment}
-            </button>
-
-            <button
-              type='button'
-              className='text-sm text-white px-4 py-2 rounded-md m-2'
-              style={{
-                backgroundColor: color ? color : DEFAULT_BACKGROUND_COLOR,
-              }}
-              onMouseEnter={handleOnMouseEnter}
-              onMouseLeave={handleOnMouseLeave}
-              onClick={handleResetCounter}
-            >
-              Reset
-            </button>
-          </div>
-        </div>
-
-        <div className='text-center text-md mb-9'>
-          <h2 className='mb-3 text-lg font-bold'>
-            {`Increment: ${increment}`}
-          </h2>
-
-          <div className='flex justify-center items-center gap-3 mb-3'>
-            <p>Select:</p>
-            <div className='flex gap-1 justify-center'>
-              {[1, 2, 3, 4, 5].map((number) => (
-                <button
-                  type='button'
-                  key={number}
-                  value={number}
-                  className='text-sm text-white w-10 h-10 rounded-full'
-                  style={{
-                    backgroundColor: color ? color : DEFAULT_BACKGROUND_COLOR,
-                  }}
-                  onMouseEnter={handleOnMouseEnter}
-                  onMouseLeave={handleOnMouseLeave}
-                  onClick={handleSelectIncrement}
-                >
-                  {number}
-                </button>
-              ))}
+          {/* Counter tracker */}
+          <section className='mb-12'>
+            <h2 className='mb-1 text-lg font-bold'>{`Counter: ${count}`}</h2>
+            <div className='flex justify-center mb-6'>
+              <button
+                type='button'
+                className='text-sm text-white px-4 py-2 rounded-md m-2'
+                style={{ backgroundColor: color || DEFAULT_BACKGROUND_COLOR }}
+                onMouseEnter={handleOnMouseEnter}
+                onMouseLeave={handleOnMouseLeave}
+                onClick={handleCounterIncreaseButton}
+              >
+                Increase +{increment}
+              </button>
+              <button
+                type='button'
+                className='text-sm text-white px-4 py-2 rounded-md m-2'
+                style={{ backgroundColor: color || DEFAULT_BACKGROUND_COLOR }}
+                onMouseEnter={handleOnMouseEnter}
+                onMouseLeave={handleOnMouseLeave}
+                onClick={handleCounterResetButton}
+              >
+                Reset
+              </button>
             </div>
-          </div>
+          </section>
 
-          <div className='flex justify-center items-center gap-3'>
-            <p>Choose custom: </p>
-
-            <input
-              type='text'
-              className='bg-gray-200 px-3 py-1 outline-none w-16'
-              placeholder='1-100'
-              name='choose-custom-increment'
-              id='id-choose-custom-increment'
-              value={customIncrement}
-              onChange={handleTextCustomIncrement}
-            />
-
-            <input
-              type='button'
-              value='Apply'
-              className={cn(
-                'text-white',
-                'px-4',
-                'py-2',
-                'rounded-md',
-                'text-sm',
-                'hover:bg-blue-600',
-                {
-                  'opacity-50': hasErrorsCustomIncrement,
-                  'cursor-not-allowed': hasErrorsCustomIncrement,
-                  'hover:bg-blue-500': hasErrorsCustomIncrement,
-                },
-              )}
-              style={{
-                backgroundColor: color ? color : DEFAULT_BACKGROUND_COLOR,
-              }}
-              onMouseEnter={handleOnMouseEnter}
-              onMouseLeave={handleOnMouseLeave}
-              onClick={handleSubmitCustomIncrement}
-              disabled={hasErrorsCustomIncrement}
-            />
-          </div>
-
-          {hasErrorsCustomIncrement && (
-            <div className='mt-3 bg-orange-100 border-l-8 border-orange-500 text-orange-700 p-2 w-[300px] text-center flex mx-auto justify-center'>
-              {errorMessageCustomIncrement}
+          {/* Custom Increment */}
+          <section>
+            <h2 className='mb-3 text-lg font-bold'>
+              {`Increment: ${increment}`}
+            </h2>
+            <div className='flex justify-center items-center gap-3 mb-3'>
+              <p>Select:</p>
+              <div className='flex gap-1 justify-center'>
+                {[1, 2, 3, 4, 5].map((number) => (
+                  <button
+                    type='button'
+                    key={number}
+                    value={number}
+                    className='text-sm text-white w-10 h-10 rounded-full'
+                    style={{
+                      backgroundColor: color || DEFAULT_BACKGROUND_COLOR,
+                    }}
+                    onMouseEnter={handleOnMouseEnter}
+                    onMouseLeave={handleOnMouseLeave}
+                    onClick={handleIncrementSelect}
+                  >
+                    {number}
+                  </button>
+                ))}
+              </div>
             </div>
-          )}
-        </div>
+            <div className='flex justify-center items-center gap-3'>
+              <p>Choose custom: </p>
+              <input
+                type='text'
+                className='bg-gray-200 px-3 py-1 outline-none w-16'
+                placeholder='1-100'
+                name='choose-custom-increment'
+                id='id-choose-custom-increment'
+                value={customIncrement}
+                onChange={handleIncrementTextInput}
+              />
+              <input
+                type='button'
+                value='Apply'
+                className={cn(
+                  'text-white',
+                  'px-4',
+                  'py-2',
+                  'rounded-md',
+                  'text-sm',
+                  'hover:bg-blue-600',
+                  {
+                    'opacity-50': hasErrorsCustomIncrement,
+                    'cursor-not-allowed': hasErrorsCustomIncrement,
+                    'hover:bg-blue-500': hasErrorsCustomIncrement,
+                  },
+                )}
+                style={{ backgroundColor: color || DEFAULT_BACKGROUND_COLOR }}
+                onMouseEnter={handleOnMouseEnter}
+                onMouseLeave={handleOnMouseLeave}
+                onClick={handleIncrementSubmit}
+                disabled={hasErrorsCustomIncrement}
+              />
+            </div>
 
-        <div className='text-center text-md'>
-          <hr class='mb-3 border-2' />
-          <h2 className='mb-3 text-lg font-bold'>{`Choose Color Theme`}</h2>
-
-          <label htmlFor='id-select-counter-color'>{`Select theme: `}</label>
-
-          <select
-            value={color}
-            onChange={handleSelectColor}
-            id='id-select-counter-color'
-            className='outline-none mb-4 w-40'
-          >
-            <option value=''>Choose color</option>
-            <option value='#795548'>Brown</option>
-            <option value='#fb8c00'>Orange</option>
-            <option value='#388e3c'>Green</option>
-            <option value='#5e35b1'>Purple</option>
-            <option value='#c62828'>Red</option>
-          </select>
-
-          <div className='flex justify-center gap-3 text-sm'>
-            <input
-              type='text'
-              name='input-text-1'
-              placeholder='Add custom color...'
-              className='bg-gray-200 px-3 py-2 w-40 outline-none'
-              value={textColor}
-              onChange={handleTextColor}
+            {/* {hasErrorsCustomIncrement && (
+              <div className='mt-3 bg-orange-100 border-l-8 border-orange-500 text-orange-700 p-2 w-[300px] text-center flex mx-auto justify-center'>
+                {errorMessageCustomIncrement}
+              </div>
+            )} */}
+            <Alert
+              isActive={hasErrorsCustomIncrement}
+              alertText={errorMessageCustomIncrement}
+              alertType={alertTypes.Warning}
             />
+          </section>
 
-            <input
-              type='button'
-              value='Update Color'
-              className={cn(
-                'text-white',
-                'px-4',
-                'py-2',
-                'rounded-md',
-                'text-sm',
-                'hover:bg-blue-600',
-                {
-                  'opacity-50': hasErrorSubmitColor || hasErrorTextColor,
-                  'cursor-not-allowed':
-                    hasErrorSubmitColor || hasErrorTextColor,
-                  'hover:bg-blue-500': hasErrorSubmitColor || hasErrorTextColor,
-                },
-              )}
-              style={{
-                backgroundColor: color ? color : DEFAULT_BACKGROUND_COLOR,
-              }}
-              onMouseEnter={handleOnMouseEnter}
-              onMouseLeave={handleOnMouseLeave}
-              onClick={handleSubmitColor}
-              disabled={hasErrorSubmitColor || hasErrorTextColor}
+          {/* Color theme */}
+          <section>
+            <h2 className='mb-3 text-lg font-bold'>{`Choose Color Theme`}</h2>
+            <label htmlFor='id-select-counter-color'>{`Select theme: `}</label>
+            <select
+              value={color}
+              onChange={handleColorSelect}
+              id='id-select-counter-color'
+              className='outline-none mb-4 w-40'
+            >
+              <option value=''>Choose color</option>
+              <option value='#795548'>Brown</option>
+              <option value='#fb8c00'>Orange</option>
+              <option value='#388e3c'>Green</option>
+              <option value='#5e35b1'>Purple</option>
+              <option value='#c62828'>Red</option>
+            </select>
+            <div className='flex justify-center gap-3 text-sm'>
+              <input
+                type='text'
+                name='input-text-1'
+                placeholder='Add custom color...'
+                className='bg-gray-200 px-3 py-2 w-40 outline-none'
+                value={textColor}
+                onChange={handleColorTextInput}
+              />
+              <input
+                type='button'
+                value='Update Color'
+                className={cn(
+                  'text-white',
+                  'px-4',
+                  'py-2',
+                  'rounded-md',
+                  'text-sm',
+                  'hover:bg-blue-600',
+                  {
+                    'opacity-50': hasErrorSubmitColor || hasErrorTextColor,
+                    'cursor-not-allowed':
+                      hasErrorSubmitColor || hasErrorTextColor,
+                    'hover:bg-blue-500':
+                      hasErrorSubmitColor || hasErrorTextColor,
+                  },
+                )}
+                style={{ backgroundColor: color || DEFAULT_BACKGROUND_COLOR }}
+                onMouseEnter={handleOnMouseEnter}
+                onMouseLeave={handleOnMouseLeave}
+                onClick={handleColorSubmit}
+                disabled={hasErrorSubmitColor || hasErrorTextColor}
+              />
+            </div>
+
+            {/* <Alert
+              isActive={hasErrorSubmitColor}
+              alertText={alertMessages.ColorNotFound}
+              alertType={alertTypes.Warning}
+            /> */}
+
+            <Alert
+              isActive={hasErrorTextColor}
+              alertText={alertMessages.ColorOnlyAlphabeticalChars}
+              alertType={alertTypes.Warning}
             />
-          </div>
-
-          <Alert
-            isActive={hasErrorSubmitColor}
-            alertText={alertMessages.ColorNotFound}
-            alertType={alertTypes.Warning}
-          />
-
-          <Alert
-            isActive={hasErrorTextColor}
-            alertText={alertMessages.ColorValidation}
-            alertType={alertTypes.Warning}
-          />
-        </div>
-      </form>
+          </section>
+        </form>
+      </main>
     </div>
   );
 }
